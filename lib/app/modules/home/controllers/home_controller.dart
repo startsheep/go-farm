eeac80b33ae42f0c06a33a0d284b4b3ca099f82e
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-11-24 15:57:44 +0700

    ⬆️⬆️⬆️Setup For home

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,9 +1,101 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_pattern_starter/app/widgets/carousel_content.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
+  RxList<Map<String, dynamic>> carouselContents = <Map<String, dynamic>>[
+    {
+      'id': 1,
+      'title': 'Pasar Domba',
+      'image': 'assets/placeholder.jpg',
+    },
+    {
+      'id': 2,
+      'title': 'Hari Raya Kurban',
+      'image': 'assets/placeholder.jpg',
+    },
+    {
+      'id': 3,
+      'title': 'Pasar Domba mingguan',
+      'image': 'assets/placeholder.jpg',
+    },
+  ].obs;
 
-  final count = 0.obs;
+  RxList<Map<String, dynamic>> farms = <Map<String, dynamic>>[
+    {
+      'id': 1,
+      'title': 'Ternak Hj Mursidin Al-Bakri',
+      'image': 'assets/placeholder.jpg',
+      'address': {
+        'street': 'Jl. Raya Cibadak',
+        'city': 'Bandung',
+        'province': 'Jawa Barat',
+      },
+    },
+    {
+      'id': 2,
+      'title': 'Ternak Hj Abul',
+      'image': 'assets/placeholder.jpg',
+      'address': {
+        'street': 'Jl. Raya Cibadak',
+        'city': 'Bandung',
+        'province': 'Jawa Barat',
+      },
+    },
+    {
+      'id': 3,
+      'title': 'Nan Farm',
+      'image': 'assets/placeholder.jpg',
+      'address': {
+        'street': 'Jl. Lohbener',
+        'city': 'Indramayu',
+        'province': 'Jawa Barat',
+      },
+    },
+  ].obs;
+
+  RxList<Map<String, dynamic>> adoptedAnimals = <Map<String, dynamic>>[
+    {
+      'id': 1,
+      'title': 'Anjing Kintamani',
+      'image': 'assets/placeholder.jpg',
+      'price': 100000,
+      'address': {
+        'street': 'Jl. Raya Cibadak',
+        'city': 'Bandung',
+        'province': 'Jawa Barat',
+      },
+    },
+    {
+      'id': 2,
+      'title': 'Kucing Persia',
+      'image': 'assets/placeholder.jpg',
+      'price': 100000,
+      'address': {
+        'street': 'Jl. Raya Cibadak',
+        'city': 'Bandung',
+        'province': 'Jawa Barat',
+      },
+    },
+    {
+      'id': 3,
+      'title': 'Kucing Anggora',
+      'image': 'assets/placeholder.jpg',
+      'price': 100000,
+      'address': {
+        'street': 'Jl. Lohbener',
+        'city': 'Indramayu',
+        'province': 'Jawa Barat',
+      },
+    },
+  ].obs;
+
+  // assign carouselContents to a carousel items
+
+  // RxList<Widget> carouselItems =
+
   @override
   void onInit() {
     super.onInit();
@@ -18,6 +110,4 @@ class HomeController extends GetxController {
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,13 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:getx_pattern_starter/app/modules/home/widgets/farm_content.dart';
+import 'package:getx_pattern_starter/app/theme/colors.dart';
+import 'package:getx_pattern_starter/app/widgets/carousel_content.dart';
+import 'package:getx_pattern_starter/app/widgets/reusetext_field.dart';
+import 'package:getx_pattern_starter/app/widgets/title_bar.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -10,13 +17,128 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text.rich(
+          TextSpan(
+            text: 'Go',
+            style: TextStyle(
+              color: AppTheme.primary,
+              fontSize: 30,
+              fontWeight: FontWeight.w600,
+            ),
+            children: [
+              TextSpan(
+                text: ' Farm',
+                style: TextStyle(
+                  color: AppTheme.secondary,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: ReuseTextField(
+                hintText: 'Search',
+                prefixIcon: Icons.search,
+                suffixIcon: Icons.filter_list,
+                onPressSuffix: () {
+                  print('filter');
+                },
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Obx(
+              () {
+                return CarouselSlider(
+                  items: controller.carouselContents
+                      .map((element) => CarouselContent(content: element))
+                      .toList(),
+                  options: CarouselOptions(
+                    height: 150.0,
+                    enlargeCenterPage: true,
+                    autoPlay: true,
+                    aspectRatio: 16 / 9,
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    // enableInfiniteScroll: true,
+                    autoPlayAnimationDuration: Duration(milliseconds: 1000),
+                    viewportFraction: 0.8,
+                  ),
+                );
+              },
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            // SingleChildScrollView()
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: TitleBar(
+                title: "Peternakan",
+                sideTittle: "Lihat semua",
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 200,
+              child: Obx(
+                () {
+                  // make gridview
+                  return GridView.builder(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 10),
+                    // reverse: ,
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      childAspectRatio: 4 / 4,
+                    ),
+                    itemCount: controller.farms.length,
+                    itemBuilder: (context, index) {
+                      final farm = controller.farms[index];
+                      return FarmContent(
+                        content: farm,
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: TitleBar(
+                title: "Gambar 360",
+                sideTittle: "Lihat semua",
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: TitleBar(
+                title: "Hewan Adopsi",
+                sideTittle: "Lihat semua",
+              ),
+            ),
+          ],
         ),
       ),
     );
