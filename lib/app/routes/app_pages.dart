48c1cf0e560d9ada08d41f3807678c50b660be7e
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-11-25 16:52:32 +0700

    🔨🔨🔨

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,13 +1,19 @@
 import 'package:get/get.dart';
 
+import '../modules/auth/bindings/auth_binding.dart';
+import '../modules/auth/views/auth_view.dart';
+import '../modules/cart/bindings/cart_binding.dart';
+import '../modules/cart/views/cart_view.dart';
 import '../modules/core/bindings/core_binding.dart';
 import '../modules/core/views/core_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/products/bindings/products_binding.dart';
+import '../modules/products/views/products_view.dart';
+import '../modules/profile/bindings/profile_binding.dart';
+import '../modules/profile/views/profile_view.dart';
 import '../modules/search/bindings/search_binding.dart';
 import '../modules/search/views/search_view.dart';
-import '../modules/view/bindings/view_binding.dart';
-import '../modules/view/views/view_view.dart';
 
 part 'app_routes.dart';
 
@@ -28,14 +34,29 @@ class AppPages {
       binding: CoreBinding(),
     ),
     GetPage(
-      name: _Paths.VIEW,
-      page: () => const ViewView(),
-      binding: ViewBinding(),
-    ),
-    GetPage(
       name: _Paths.SEARCH,
       page: () => const SearchView(),
       binding: SearchBinding(),
+    ),
+    GetPage(
+      name: _Paths.AUTH,
+      page: () => const AuthView(),
+      binding: AuthBinding(),
+    ),
+    GetPage(
+      name: _Paths.PRODUCTS,
+      page: () => const ProductsView(),
+      binding: ProductsBinding(),
+    ),
+    GetPage(
+      name: _Paths.PROFILE,
+      page: () => const ProfileView(),
+      binding: ProfileBinding(),
+    ),
+    GetPage(
+      name: _Paths.CART,
+      page: () => const CartView(),
+      binding: CartBinding(),
     ),
   ];
 }
--- lib/app/theme/colors.dart
+++ lib/app/theme/colors.dart
@@ -7,13 +7,21 @@ class AppTheme {
   // make class for theme data
   static Color primary = Color.fromARGB(255, 75, 255, 117);
   static Color secondary = Color.fromARGB(228, 0, 222, 100);
-  static Color tertiary = Color.fromARGB(255, 0, 222, 141);
-  static Color quaternary = Color.fromARGB(255, 0, 222, 141);
+  static Color success = Color.fromARGB(255, 119, 255, 119);
+  static Color warning = Color.fromARGB(255, 255, 255, 117);
+  static Color danger = Color.fromARGB(255, 255, 107, 107);
   static Color dIconColor = Color.fromARGB(255, 178, 178, 178);
+  static Color gray = Color.fromARGB(255, 127, 127, 127);
   static Color dTextColor = Color.fromARGB(255, 0, 0, 0);
   static Color accent = Color.fromARGB(255, 191, 191, 191);
   static Color errorContainer = Color.fromARGB(255, 255, 104, 104);
   static Color defaultBackground = Color.fromARGB(255, 249, 249, 249);
+  static BoxShadow dShadow = BoxShadow(
+    color: Color.fromARGB(255, 237, 237, 237).withOpacity(0.5),
+    spreadRadius: 2,
+    blurRadius: 2,
+    offset: Offset(2, 2),
+  );
   static ThemeData themeData = ThemeData(
     primaryColor: primary,
     scaffoldBackgroundColor: defaultBackground,
